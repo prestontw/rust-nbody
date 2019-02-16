41a119a642262c45a464557d64f292a45f9d4ff7
Preston TW <preston_tunnell_wilson@brown.edu>
2019-02-16 00:42:07 -0500

    add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
+//! Implementing the N-body program in Rust (with a little bit of EC(S) thrown in).
+//! 
+//! This is almost a direct translation from an assignment in a Parallel Systems
+//! course I took algorithm-wise.
+//! The interesting part of that assignment is that we had to use UPC—the
+//! interesting parts of this implementation are:
+//! * it's in Rust,
+//! * it's a little optimized, and
+//! * it has the data layout of an EC(S) implementation.
+
 use rayon::prelude::*;
 
 const N: usize = 10000;
 const G: f64 = 6.67e-11;
 const TIMESTEP: f64 = 0.25;
+/// The number of steps to simulate.
 pub const NSTEPS: usize = 10;
 
 struct Position {
@@ -26,20 +37,35 @@ struct Acceleration {
   az: f64,
 }
 
+/// The main structure of this program.
+/// The state for a body is split across
+/// a position vector,
+/// a velocities vector,
+/// and a mass vector.
+/// 
+/// Originally, this was implemented as an array of structures—now
+/// it's a structure of arrays.
+/// This was both for testing optimizations and for minute practice with EC(S).
 pub struct BodyStates {
   poss: Vec<Position>,
   vels: Vec<Velocity>,
   masses: Vec<f64>,
 }
 
+/// Given a point from the origin as `f64`'s,
+/// calculate its distance from the origin.
 fn dist(dx: f64, dy: f64, dz: f64) -> f64 {
   ((dx * dx) + (dy * dy) + (dz * dz)).sqrt()
 }
 
+/// Given the masses of two bodies and the distance between them,
+/// calculate the force between them.
 fn force(mass1: f64, mass2: f64, distance: f64) -> f64 {
   (G * mass1 * mass2) / (distance * distance)
 }
 
+/// Given the position and mass of one body,
+/// calculate the force acting on it from all of the other bodies.
 fn forces_for_body<'a, I>(p: &Position, m: f64, reference: I) -> Force
 where
   I: rayon::iter::IntoParallelIterator<Item = (&'a Position, &'a f64)>,
@@ -74,6 +100,7 @@ where
     )
 }
 
+/// Calculate the accelerations for these bodies—used to update the bodies' velocities.
 fn accelerations(bs: &BodyStates) -> Vec<Acceleration> {
   bs.poss
     .par_iter()
@@ -92,7 +119,7 @@ fn accelerations(bs: &BodyStates) -> Vec<Acceleration> {
     .collect()
 }
 
-// maybe make this update the position?
+/// Returns a new `Position` from a `Position` moving at a certain `Velocity`.
 fn move_position(p: &Position, v: &Velocity) -> Position {
   Position {
     x: p.x + v.dx * TIMESTEP,
@@ -101,7 +128,7 @@ fn move_position(p: &Position, v: &Velocity) -> Position {
   }
 }
 
-// maybe make this statefully update vel?
+/// Returns a new `Velocity` from a `Velocity` accelerating at a certain `Acceleration`.
 fn update_velocity(v: &Velocity, a: &Acceleration) -> Velocity {
   Velocity {
     dx: v.dx + a.ax * TIMESTEP,
@@ -110,7 +137,7 @@ fn update_velocity(v: &Velocity, a: &Acceleration) -> Velocity {
   }
 }
 
-// slight slowdown from making not parallel
+/// Computes the next `BodyStates`.
 pub fn compute_forces(bs: BodyStates) -> BodyStates {
   let accs = accelerations(&bs);
   BodyStates {
@@ -130,6 +157,8 @@ pub fn compute_forces(bs: BodyStates) -> BodyStates {
   }
 }
 
+/// Simple function to create a lot of bodies.
+/// Thank you, Larkins, for letting me use these umbers.
 pub fn init<'a>() -> BodyStates {
   let range: Vec<f64> = (0..N).map(|i| i as f64).collect();
   let ret = BodyStates {
